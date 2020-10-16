6186dfa1ad416a564fa0e0684558872b47ebbfdd
Rom Grk <romgrk.cc@gmail.com>
2020-10-16 13:45:34 -0400

    install: update

--- bash/init.sh
+++ bash/init.sh
@@ -8,6 +8,9 @@ case $- in
       *) return;;
 esac
 
+export PATH=PATH:$HOME/bin
+
+
 # don't put duplicate lines or lines starting with space in the history.
 # See bash(1) for more options
 HISTCONTROL=ignoreboth
