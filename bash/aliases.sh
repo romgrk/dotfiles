a1e341b80ea3b260e34d07c466a1b1a33206a18c
Rom Grk <romgrk.cc@gmail.com>
2020-10-16 14:18:08 -0400

    aliases: update

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -11,20 +11,25 @@ alias grep="grep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn}"
 
 alias g="git"
 alias gk="git checkout"
-alias clone="git clone"
 alias gst='git status'
-alias amen='git commit --amen'
+alias glo="git log --oneline"
+alias glogline="git log --oneline"
+alias gl='git pull'
+alias gp='git push'
+alias gd='git diff'
 alias gds='git diff --staged'
 alias gss='git stash'
 alias gsp='git stash pop'
 alias gr='git rebase'
-alias gl='git pull'
-alias gp='git push'
 alias gpf='git push --force'
 alias gpu='git push -u origin `git rev-parse --abbrev-ref HEAD`'
 alias gcm='git commit -m'
-alias gcam='git commit -am'
-alias gaa='git add -all'
+alias gaa='git add --all'
+alias gcm='git commit -m'
+alias gds='git diff --staged'
+alias gss='git stash'
+alias gsp='git stash pop'
+alias gpf='git push --force'
 
 alias npmy='npm init --yes'
 alias npmi='npm install --save'
